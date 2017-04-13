f4e0d5ca97ddf3eef08d41c01456631cecb1aba7
Wilmord <berkayelbir10@hotmail.com>
2017-04-13 22:46:08 +0300

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,19 +1,29 @@
+/****************************
+* @Author: Berkay Elbir
+**************************/
+
 #pragma once
 #include <vector>
 #include <list>
 
 // A class to represent a graph. A graph is a array of adjacency lists.
-// Size of vector will be nodeNumber (number of vertices in graph)
+// Size of list will be nodeNumber (number of vertices in graph)
 class Graph
 {
 public:
 	Graph(const int nodeNumber);
 	~Graph();
+	Graph(Graph& other);
+	Graph(Graph&& other);
+	Graph& operator=(Graph& other);
+	Graph& operator=(Graph&& other);
 	void addEdge(int src, int dest);
 	void printGraph() const; 	// A utility function to print the adjacenncy list representation of graph
 	const std::list<int>* getNodeList() const;
 	const int getNodeCount() const;
 private:
+	void copyListFrom(Graph& other);
+private:
 	int numberOfNodes;
-	std::list<int> *adj;
+	std::list<int>* adj;
 };
